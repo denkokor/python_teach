afcb9c905abcdd285612b6dd54cfdf07eea1e7c6
denkokor <106378612+denkokor@users.noreply.github.com>
2022-05-29 09:38:42 +0300

    pycharm

--- hero.py
+++ hero.py
@@ -1,12 +1,6 @@
-class Question:
-    def __init__(self, question, answer):
-        self.question = question
-        self.answer = answer
 
-    def check(self, answer):
-        return answer == self.answer
+class Fish():
+    def swim(self):
+        print("Я плаваю")
 
-question_1 = Question("Текст вопроса?", "правильный ответ")
-print(question_1.check("test"))
-print(question_1.check("правильный ответ"))
-print()
+        
